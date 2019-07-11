66a0f05272538b772832cd6505bbc0ca926626eb
A.S. Lomoff <lomov.as@gmail.com>
2019-07-11 17:53:15 +0300

    Make duty_manager a singleton

--- duty_manager.rb
+++ duty_manager.rb
@@ -2,6 +2,10 @@ class DutyManager
   attr_reader :store
   NUMBERS = %w[3467 3536 3532 3460 3459 3573 3529 3488 3486 3547 3476 3531 3523 3457 3626 3473 3574 3688 3638 3541 3570 3657 3512 3444 3715 3711 3640 3729 3123 3496 3538 3520 3545 3663]
 
+  def self.instance
+    @instance ||= self.new
+  end
+
   def initialize
     @store = {}
   end
