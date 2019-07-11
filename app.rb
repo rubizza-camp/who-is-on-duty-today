5c781a138ee8ad39054820918c8e8e1491055d67
A.S. Lomoff <lomov.as@gmail.com>
2019-07-11 17:54:09 +0300

    add protected and public clusses to app.rb to protect duties endpoint with basic auth

--- app.rb
+++ app.rb
@@ -1,14 +1,21 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'date'
 require 'json'
 require './duty_manager'
 
-duty_manager = DutyManager.new
+class Protected < Sinatra::Base
+  use Rack::Auth::Basic, "Protected Area" do |username, password|
+    username == ENV['BASIC_AUTH_USERNAME'] &&
+    password == ENV['BASIC_AUTH_PASSWORD']
+  end
 
-get '/' do
-  "Todays dutyman is #{duty_manager.who_is_on_duty?}"
+  get '/duties' do
+    DutyManager.instance.store.to_json
+  end
 end
 
-get '/duties' do
-  duty_manager.store.to_json
+class Public < Sinatra::Base
+  get '/' do
+    "Todays dutyman is #{DutyManager.instance.who_is_on_duty?}"
+  end
 end
